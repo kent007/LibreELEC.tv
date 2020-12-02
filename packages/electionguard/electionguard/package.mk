315f7836fdd2944816ebc447af348759add05913
KnowLim <stevenl4@vt.edu>
2020-12-02 16:29:35 -0500

    Added new electionguard

--- packages/electionguard/electionguard/package.mk
+++ packages/electionguard/electionguard/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="electionguard"
-PKG_VERSION="1.0.0"
-PKG_SHA256="455579c528d5990207e2225d3ff13e6e07f1579fba774b1f26f0a4c0551c0217"
+PKG_VERSION="2.0.0"
+PKG_SHA256="ca755da180a5af0ceedb802fc023a07eb2dda08debf8ace507e9e383b549d288"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/tausiftt5238/cs5204_election_guard"
-PKG_URL="https://github.com/tausiftt5238/cs5204_election_guard/releases/download/$PKG_VERSION/$PKG_NAME.tar.gz"
+PKG_URL="https://github.com/tausiftt5238/cs5204_election_guard/releases/download/v$PKG_VERSION/$PKG_NAME.tar.gz"
 PKG_DEPENDS_HOST="Python3:host"
 PKG_LONGDESC="Its used by electionguard"
 PKG_TOOLCHAIN="manual"
